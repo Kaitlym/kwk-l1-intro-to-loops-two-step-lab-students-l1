f58297679ccfa300d32360130df5a3149c000c27
Kaitlym <azkatie2014@gmail.com>
2018-06-06 17:38:54 +0000

    automatically backed up by learn

---
+++ hotel.rb
@@ -0,0 +1,5 @@
+def hotel_welcome(name, hotel, room_number)
+  puts"Welcome to #{hotel} room #{room_number}.
+Have a nice stay #{name}!"
+end
+hotel_welcome("Name", "Hotel Name", "Room Number")
---
+++ loops.rb
@@ -0,0 +1,36 @@
+#10.times do
+  #puts "where the food"
+#end
+#loop do
+  #puts "grumble" 
+  #puts "I'm SO hungry"
+  #puts "=("
+#end
+
+# def dance 
+#   moves_slay = 0
+#   loop do
+#     if moves_slay == 10
+#       break
+#     end
+#     puts "step to the left"
+#      puts "step to the right"
+#     puts "kick"
+#     moves_slay = moves_slay+1
+#   end
+# end
+# # dance
+
+# def kwk_camp (count_down)
+#   loop do
+#     if count_down == 0
+#       break
+#     end
+#     puts "go to code with klossy. #{count_down} days left"
+#     count_down=count_down-1
+#     sleep(0.5)
+#   end
+#   puts "congrats you finished code with klossy!"
+# end
+# kwk_camp(10)
+  
